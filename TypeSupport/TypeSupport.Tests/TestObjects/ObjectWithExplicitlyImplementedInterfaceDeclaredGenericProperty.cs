5ac9db88e91dba513de736aae7796d0660a64108
Steven T. Cramer <SteveTCramer@gmail.com>
2019-06-20 05:55:11 +0000

    Add Test case to duplicate exception
    This is the minimum I could come up with to reproduce.

---
+++ TypeSupport/TypeSupport.Tests/TestObjects/ObjectWithExplicitlyImplementedInterfaceDeclaredGenericProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace TypeSupport.Tests.TestObjects
+{
+
+    public interface IValue<T>
+    {
+        T Value { get; }
+    }
+
+    public class ObjectWithExplicitlyImplementedInterfaceDeclaredGenericProperty<T> : IValue<T>
+    {
+        T IValue<T>.Value { get; }
+    }
+}
