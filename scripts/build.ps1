9f3219c74d63b967bb0dc9edb7ad00689e6db3ab
Michael Brown <mikeb@360replays.com>
2020-07-09 18:37:55 -0700

    adds source snupkg file to build artifacts and deployment

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,6 +3,8 @@ Write-Host "Building..." -ForegroundColor green
 dotnet build .\TypeSupport\TypeSupport.sln | Select-String -Pattern " warning " -NotMatch | Select-String -Pattern "INFO:" -NotMatch | Select-String -Pattern "WARNING:" -NotMatch
 
 Write-Host "Packaging..." -ForegroundColor green
-dotnet pack .\TypeSupport\TypeSupport\TypeSupport.csproj --configuration Release --no-build
+dotnet pack .\TypeSupport\TypeSupport\TypeSupport.csproj --configuration Release --no-build -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
-Get-ChildItem .\*.nupkg -recurse | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+Get-ChildItem .\*.nupkg -recurse | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name -Type NuGetPackage }
+
+Get-ChildItem .\*.snupkg -recurse | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name -Type NuGetPackage }
